6b5f1777c2e5a21c35a6e527827bbeef5ddef38b
e-koch <koch.eric.w@gmail.com>
2014-10-24 10:37:12 -0600

    Create download spectra list.

--- project_code/download_spectra.py
+++ project_code/download_spectra.py
@@ -4,9 +4,9 @@ Download SDSS spectra from the plate ID and fibre
 '''
 
 import os
+from astropy.io import fits
 
-
-def download_spectra(plateid, fibreid, mjd, survey):
+def download_spectra(plateid, fibreid, mjd, survey, download=False):
     '''
     Download an SDSS spectrum given the parameters.
     '''
@@ -32,10 +32,48 @@ def download_spectra(plateid, fibreid, mjd, survey):
     else:
         raise NameError("Check the survey name. Inputted was %s" % (survey))
 
-    # Check to make sure that file exists
-    os.system("wget http://data.sdss3.org/sas/dr10/"+file_suffix)
+    if download:
+        # Check to make sure that file exists
+        os.system("wget http://data.sdss3.org/sas/dr10/"+file_suffix)
 
-    return filename
+        try:
+            fits.open(file_suffix)
+        except IOError:
+            pass
+
+        return filename
+
+    else:
+        return file_suffix
+
+
+def download_list(obs_file, output_file):
+    '''
+    Makes a txt file with the locations of the files to download.
+    '''
+
+    # Open the file
+    data_file = fits.open(obs_file)
+
+    spectra_data = data_file[1].data
+    del data_file
+
+    num_spectra = spectra_data.size
+
+    output = open(output_file, 'a')
+
+    for i in range(num_spectra):
+        spec_info = spectra_data[i]
+
+        # Download the spectrum
+        spec_name = \
+            download_spectra(spec_info['PLATE'], spec_info['FIBERID'],
+                             spec_info['MJD'], spec_info['SURVEY'],
+                             download=False)
+
+        output.write("%s\n" % spec_name)
+
+    output.close()
 
 
 def split_spectra_list(props_file, n_split=4):
