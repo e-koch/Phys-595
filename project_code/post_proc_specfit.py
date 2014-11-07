d716c9195ad2a63cd8748366aeb58af55a8b4c0e
e-koch <koch.eric.w@gmail.com>
2014-11-07 16:59:48 -0700

    Set elements to 0

--- project_code/post_proc_specfit.py
+++ project_code/post_proc_specfit.py
@@ -28,8 +28,8 @@ def blank_the_crap(filename, min_amp_sn=3, min_wid_sn=3):
              data.iloc[i+22][1:],
              data.iloc[i+33][1:]]
 
-        good_err = np.logical_and(line_pars[1] > 0,
-                             line_pars[3] > 0)
+        good_err = np.logical_or(line_pars[1] > 0,
+                                  line_pars[3] > 0)
 
         good_err = np.append([1], good_err)
 
@@ -40,10 +40,10 @@ def blank_the_crap(filename, min_amp_sn=3, min_wid_sn=3):
         good_sn_wid = np.append([1], good_sn_wid)
 
 
-        data_copy[i] = data_copy[i][(good_err + good_sn_amp + good_sn_wid) == 1]
-        data_copy[i+11] = data_copy[i+11][(good_err + good_sn_amp + good_sn_wid) == 1]
-        data_copy[i+22] = data_copy[i+22][(good_err + good_sn_amp + good_sn_wid) == 1]
-        data_copy[i+33] = data_copy[i+33][(good_err + good_sn_amp + good_sn_wid) == 1]
+        data_copy[i][(good_err + good_sn_amp + good_sn_wid) == 0] = 0.0
+        data_copy[i+11][(good_err + good_sn_amp + good_sn_wid) == 0] = 0.0
+        data_copy[i+22][(good_err + good_sn_amp + good_sn_wid) == 0] = 0.0
+        data_copy[i+33][(good_err + good_sn_amp + good_sn_wid) == 0] = 0.0
 
 
     data_copy.to_csv(filename[:-4] + "_cleaned.csv")
